99eb0f167390333141ffe3f494980bc02fa53800
ajrae_remote <ajrae.nv@gmail.com>
2020-04-13 19:11:28 -0400

    Added install.sh as temporary installation script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# create virtual environment requires python3-venv
+python3 -m venv venv
+# source virtual environment
+. venv/bin/activate
+# install required python libraries to environment
+pip install < requirements.txt
