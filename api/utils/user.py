caf3ae5e67463e231ce9df39f302ab94c00ae85d
yusef-zia <yusef.a.zia@gmail.com>
2020-05-03 17:22:55 -0400

    Making firstname an optional argument for initializing User objects.

--- api/utils/user.py
+++ api/utils/user.py
@@ -1,12 +1,12 @@
 # Interface class to represent a user. 
-# A User object must be initialized with a name and email
+# A User object must be initialized with an email
 
 # email: the unique identifier (i.e. email) describing the User
 # firstName: the User's first name
 # lastName: (optional) the user's last name
 # password: (optional) the encoded password used to authenticate the User
 class User:
-    def __init__(self, email, firstName, lastName = None, password = None):
+    def __init__(self, email, firstName = None, lastName = None, password = None):
         self.email = email
         self.firstName = firstName
         self.lastName = lastName
