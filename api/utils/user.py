2b592df1cdddc5bf4355f6fb664bb11272fe7056
yusef-zia <yusef.a.zia@gmail.com>
2020-05-03 17:12:51 -0400

    Added a User class in utils for interactions with the Mongo Interface and Flask logic.

---
+++ api/utils/user.py
@@ -0,0 +1,13 @@
+# Interface class to represent a user. 
+# A User object must be initialized with a name and email
+
+# email: the unique identifier (i.e. email) describing the User
+# firstName: the User's first name
+# lastName: (optional) the user's last name
+# password: (optional) the encoded password used to authenticate the User
+class User:
+    def __init__(self, email, firstName, lastName = None, password = None):
+        self.email = email
+        self.firstName = firstName
+        self.lastName = lastName
+        self.password = password
