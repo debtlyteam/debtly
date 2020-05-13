827aa73a91984e249313c82504271ef5ac256610
yusef-zia <yusef.a.zia@gmail.com>
2020-05-13 18:16:50 -0400

    Added an accessor for a user's unique id number

--- api/tests/database/userTest.py
+++ api/tests/database/userTest.py
@@ -1,7 +1,7 @@
 # Quick tests for using the mongo database
 
 from utils.user import User
-from database.interface import add_user, get_user, delete_user
+from database.interface import add_user, get_user, get_user_id, delete_user
 import mongoengine
 
 mongoengine.connect('test')
@@ -14,6 +14,9 @@ print(add_user(user))
 
 print("Getting User email...")
 print(get_user(user.email).email)
+
+print("Getting User ID Number...")
+print(get_user_id(user.email))
 
 print("Deleting user")
 print(delete_user(user.email))
