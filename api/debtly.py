2abd2b24502b75ca17e1e7d097fa7ab335447017
ajrae_remote <ajrae.nv@gmail.com>
2020-05-14 13:00:13 -0400

    Added Flask-Session to requirements.

--- api/debtly.py
+++ api/debtly.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 app.secret_key = b'_8H@jhAsDFh9kjd((!jf'
 app.config['SESSION_TYPE'] = 'mongodb'
 app.config['SESSION_MONGODB_DB'] = 'debtly'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
 Session(app)
 
